9538c956b2ea224f64c19d7090b300c9223bc20e
Tushar <tusharkathuria41051@gmail.com>
2024-08-02 14:22:22 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spotify_clone/',  // Set this to match your repository name
+  base: '/spotify-clone/',  // Set this to match your repository name
 })
